731cc9bfa308f9e6236440c58874ce38adf40dbe
Euller Macena <49864491+eullerm@users.noreply.github.com>
2021-07-10 13:07:02 -0300

    Mudanças no print e verificado timeout
    Realizado modificações na exibição das portas abertas, filtradas/timeout e fechadas.

--- scanner.py
+++ scanner.py
@@ -1,37 +1,63 @@
 import sys
 import socket
 
-servidor = socket.gethostbyname(sys.argv[1]) 
-min = int(sys.argv[2])
-max = int(sys.argv[3])
-
-print("+" * 50)
-print("Escaneando Servidor: " + servidor)
-print("+" * 50)
+portasAbertas = []
+portasFechadas = []
+portasFiltradas = []
 
 try:
-      
+    servidor = socket.gethostbyname(sys.argv[1]) 
+    min = int(sys.argv[2])
+    max = int(sys.argv[3])
+
+    print("Escaneando o servidor {} no intervalo [{}, {}].".format(servidor, min, max))
+
+    print()
     # O programa irá escanear portas de min até max+1
-    for porta in range(min,max+1):
+    for porta in range(min, max + 1):
+        print("Verificando a porta {}".format(porta))
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(1)
           
-        resultado = s.connect_ex((servidor,porta))
+        resultado = s.connect_ex((servidor, porta))
+        
         if resultado == 0:
-            print("Porta {} está aberta".format(porta))
-        elif resultado == 10061:  
-            print('Porta {} está filtrada, connect_ex retornou: '.format(porta)+str(resultado)) 
+            portasAbertas.append(porta)
+        
         elif resultado == 10035:  
-            print('Porta {} está fechada, connect_ex retornou: '.format(porta)+str(resultado)) 
-        else: print('Erro desconhecido'+str(resultado)) 
+            portasFechadas.append(porta)
+
+        elif resultado == 10061 or resultado == 10060:
+            portasFiltradas.append(porta)
+        
         s.close()
-          
+
+ 
+    
+    print("\n       Portas abertas: {}".format(len(portasAbertas)))
+    
+    print("-" * 34)
+    for porta in portasAbertas:
+        print("| O status porta {} é aberta. |".format(porta))
+    print("-" * 34)
+    
+    print("\n       Portas fechadas: {}".format(len(portasFechadas)))
+    
+    print("-" * 34)
+    for porta in portasFechadas:
+        print("| O status da porta {} é fechada. |".format(porta))
+    print("-" * 34)
+    
+    print("\n       Portas filtradas: {}".format(len(portasFiltradas)))
+    
+    print("-" * 34)
+    for porta in portasFiltradas:
+        print("| O status da porta {} é filtrada. |".format(porta))
+    print("-" * 34)
+
 except KeyboardInterrupt:
-    print("\n Saindo do programa")
+    print("\n Programa finalizado.")
     exit()
 except socket.gaierror:
-    print("\n Hostname não pode ser resolvido")
-    exit()
-except socket.error:
-    print("\n Servidor não está respondendo")
+    print("\n Entrada inválida para o nome do host.")
     exit()
