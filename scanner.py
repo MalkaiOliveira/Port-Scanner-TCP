77add8b7ad36ccb57a914065d639ccbe529450ce
Euller Macena <49864491+eullerm@users.noreply.github.com>
2021-07-10 15:16:58 -0300

    Multithreads
    Adicionado multithreads para verificar as portas.

--- scanner.py
+++ scanner.py
@@ -1,63 +1,94 @@
 import sys
 import socket
+import threading
+from queue import Queue
 
 portasAbertas = []
 portasFechadas = []
 portasFiltradas = []
+resultadoDasPortas = []
+filaDePortas = Queue()
 
-try:
-    servidor = socket.gethostbyname(sys.argv[1]) 
-    min = int(sys.argv[2])
-    max = int(sys.argv[3])
+def threader(servidor, tid):
+    while True:
+        porta = filaDePortas.get()
+        verificaPorta(servidor, porta, tid)
+        filaDePortas.task_done()
 
-    print("Escaneando o servidor {} no intervalo [{}, {}].".format(servidor, min, max))
-
-    print()
+def verificaPorta(servidor, porta, tid):
+    
     # O programa irá escanear portas de min até max+1
-    for porta in range(min, max + 1):
-        print("Verificando a porta {}".format(porta))
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(1)
-          
-        resultado = s.connect_ex((servidor, porta))
-        
-        if resultado == 0:
-            portasAbertas.append(porta)
-        
-        elif resultado == 10035:  
-            portasFechadas.append(porta)
+    print("Verificando a porta {} com a thread {}".format(porta, tid))
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # socket.setdefaulttimeout(1)
+    
+    resultado = s.connect_ex((servidor, porta))
+    
+    print("RESULTADO {}".format(resultado))
+    if resultado not in resultadoDasPortas:
+        resultadoDasPortas.append(resultado)
 
-        elif resultado == 10061 or resultado == 10060:
-            portasFiltradas.append(porta)
+    if resultado == 0:
+        portasAbertas.append(porta)
+    
+    elif resultado == 10035:  
+        portasFechadas.append(porta)
+
+    elif resultado == 10060 or resultado == 10061:
+        portasFiltradas.append(porta)
+    
+    s.close()
+
+
+def main():
+    try:
+        servidor = socket.gethostbyname(sys.argv[1]) 
+        min = int(sys.argv[2])
+        max = int(sys.argv[3])
+
+        print("Escaneando o servidor {} no intervalo [{}, {}].".format(servidor, min, max))
         
-        s.close()
+        for x in range(8):
+            thread = threading.Thread(target=threader, args=(servidor, x,), daemon=True)
+            thread.start()
+        
+        for porta in range(min, max + 1):
+            
+            filaDePortas.put(porta)
+        
+        filaDePortas.join()
 
- 
-    
-    print("\n       Portas abertas: {}".format(len(portasAbertas)))
-    
-    print("-" * 34)
-    for porta in portasAbertas:
-        print("| O status porta {} é aberta. |".format(porta))
-    print("-" * 34)
-    
-    print("\n       Portas fechadas: {}".format(len(portasFechadas)))
-    
-    print("-" * 34)
-    for porta in portasFechadas:
-        print("| O status da porta {} é fechada. |".format(porta))
-    print("-" * 34)
-    
-    print("\n       Portas filtradas: {}".format(len(portasFiltradas)))
-    
-    print("-" * 34)
-    for porta in portasFiltradas:
-        print("| O status da porta {} é filtrada. |".format(porta))
-    print("-" * 34)
+        print("\n       Portas abertas: {}".format(len(portasAbertas)))
+        
+        print("-" * (34 + len(str(max))))
+        portasAbertas.sort()
+        for porta in portasAbertas:
+            print("| O status porta {} é aberta. |".format(porta))
+        print("-" * (34 + len(str(max))))
+        
+        print("\n       Portas fechadas: {}".format(len(portasFechadas)))
+        
+        print("-" * (34 + len(str(max))))
+        portasFechadas.sort()
+        for porta in portasFechadas:
+            print("| O status da porta {} é fechada. {} |".format(porta))
+        print("-" * (34 + len(str(max))))
+        
+        print("\n       Portas filtradas: {}".format(len(portasFiltradas)))
+        
+        print("-" * (34 + len(str(max))))
+        portasFiltradas.sort()
+        for porta in portasFiltradas:
+            print("| O status da porta {} é filtrada. |".format(porta))
+        print("-" * (34 + len(str(max))))
 
-except KeyboardInterrupt:
-    print("\n Programa finalizado.")
-    exit()
-except socket.gaierror:
-    print("\n Entrada inválida para o nome do host.")
-    exit()
+        print("Erros recebidos: {}".format(resultadoDasPortas))
+
+    except KeyboardInterrupt:
+        print("\n Programa finalizado.")
+        exit()
+    except socket.gaierror:
+        print("\n Entrada inválida para o nome do host.")
+        exit()
+
+main()
